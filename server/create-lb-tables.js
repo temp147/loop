47ed42f0dd512a8a9117a8e5d2bd4846cf2671f1
temp147 <temp147@163.com>
2015-12-24 14:36:21 +0800

    First Commit

---
+++ server/create-lb-tables.js
@@ -0,0 +1,12 @@
+/**
+ * Created by openerp on 12/24/15.
+ */
+
+var server = require('./server');
+var ds = server.dataSources.mysqlDBs;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Looback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
